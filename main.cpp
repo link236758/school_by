b481508e5e521b8139390e596591b9e6e7243e46
hitruga <154992133+link236758@users.noreply.github.com>
2024-05-15 18:58:51 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Header.h"
+#include "Struct.h"
+#include "name.h"
+#include "Age.h"
+#include "Color.h"
+#include "School.h"
+
+int main()
+{
+	Name("denis", "dmitriev");
+	Age(15);
+	School(258);
+	Color("RED");
+}
+
